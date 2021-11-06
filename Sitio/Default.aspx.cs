c7eecf89bd16da5cecef4eec33f9929b2dc9cfee
rafael.atias <22062169+rafael-atias@users.noreply.github.com>
2021-11-06 16:03:03 -0300

    creada página Default

---
+++ Sitio/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RefServicio;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            ServicioEFSoapClient servicio = new ServicioEFSoapClient();
+
+            List<Noticias> noticias = servicio.MostrarNoticiasUltimosCincoDias().ToList<Noticias>();
+
+            Session["noticias"] = noticias;
+
+            grdNoticias.DataSource = noticias;
+            grdNoticias.DataBind();
+        }
+    }
+}
