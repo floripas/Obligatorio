5df6e78d210f5ab8e8d92226b71e58435acd2b14
rafael.atias <22062169+rafael-atias@users.noreply.github.com>
2021-11-16 19:18:59 -0300

    corregir operación LogicaModelo.MostrarNoticiasUltimosCincoDias
    Se invocó la operación OEcontext.Noticias.Include para forzar la carga diligente de los datos de las noticias

--- Servicio/App_Code/LogicaModeloEF.cs
+++ Servicio/App_Code/LogicaModeloEF.cs
@@ -321,7 +321,26 @@ public class LogicaModeloEF
 
         DateTime haceCincoDias = hoy.AddDays(-5);
 
+        /**
+         * Aquí la operación Include se utiliza para indicar al contexto
+         * que se carguen los datos de las Noticias de forma diligente,
+         * tal como se explica en el siguiente enlace: 
+         * 
+         * https://docs.microsoft.com/en-us/ef/ef6/querying/related-data#eagerly-loading
+         * 
+         * Debido a que se eliminó la doble navegabilidad del modelo para
+         * evitar referencias circulares, esta es la única forma de forzar
+         * al contexto de cargar las Noticias con los Periodistas
+         * 
+         * Luego utilicé Include para Empleados y Secciones para seguir
+         * el mismo criterio.
+         * 
+         * Rafael, 16/11/2021
+         */
         return OEcontext.Noticias
+            .Include("Periodistas")
+            .Include("Empleados")
+            .Include("Secciones")
             .Where(noticia => noticia.FechaPublicacion >= haceCincoDias)
             .ToList<Noticias>();
     }
