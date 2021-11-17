c779165b27cef345db1cd50ab3c30effe676c061
rafael.atias <22062169+rafael-atias@users.noreply.github.com>
2021-11-17 20:37:56 -0300

    Cambiar nombre de clase de servicio en el sitio
    Ahora la clase se llama RefServicio.ServicioEF en todo el proyecto

--- Sitio/ABMSecciones.aspx.cs
+++ Sitio/ABMSecciones.aspx.cs
@@ -18,7 +18,7 @@ public partial class ABMSecciones : System.Web.UI.Page
     {
         try
         {
-            Secciones _unaSeccion = new ServicioEFSoapClient().BuscarSeccion(txtCodigoSeccion.Text.Trim());
+            Secciones _unaSeccion = new ServicioEF().BuscarSeccion(txtCodigoSeccion.Text.Trim());
 
             if (_unaSeccion == null)
             {
@@ -81,7 +81,7 @@ public partial class ABMSecciones : System.Web.UI.Page
 
         try
         {
-            new ServicioEFSoapClient().AltaSeccion(S);
+            new ServicioEF().AltaSeccion(S);
 
             lblMensaje.Text = "Alta con Exito";
 
@@ -108,7 +108,7 @@ public partial class ABMSecciones : System.Web.UI.Page
         {
             Secciones _unaS = (Secciones)Session["Seccion"];
 
-            new ServicioEFSoapClient().EliminarSeccion(_unaS);
+            new ServicioEF().EliminarSeccion(_unaS);
 
             lblMensaje.Text = "Baja con Exito";
 
@@ -137,7 +137,7 @@ public partial class ABMSecciones : System.Web.UI.Page
             _unaS.CodigoSeccion = txtCodigoSeccion.Text.Trim();
             _unaS.Nombre = txtNombreSeccion.Text.Trim();
 
-            new ServicioEFSoapClient().ModificarSeccion(_unaS);
+            new ServicioEF().ModificarSeccion(_unaS);
 
             lblMensaje.Text = "Modificación con Exito";
 
