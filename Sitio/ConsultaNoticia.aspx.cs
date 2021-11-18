7cebe91bbe692b04a19d1c852dd3dd65634c9975
rafael.atias <22062169+rafael-atias@users.noreply.github.com>
2021-11-18 16:23:26 -0300

    Modificar ConsultarNoticia.aspx con UserControl
    Fueron eliminados todos los elementos de esta página, pues el UserControl MostrarNoticia se encarga de todo

--- Sitio/ConsultaNoticia.aspx.cs
+++ Sitio/ConsultaNoticia.aspx.cs
@@ -11,26 +11,5 @@ public partial class ConsultaNoticia : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        try
-        {
-            Noticias noticia = (Noticias)Session["noticiaSeleccionada"];
-
-            TxtCodigo.Text = noticia.Codigo;
-            TxtTitulo.Text = noticia.Titulo;
-            TxtCuerpo.Text = noticia.Cuerpo;
-            TxtFecha.Text = noticia.FechaPublicacion.ToShortDateString();
-            TxtImportancia.Text = noticia.Importancia.ToString();
-            TxtSeccion.Text = noticia.Secciones.Nombre;
-
-            foreach (Periodistas periodista in noticia.Periodistas)
-            {
-                LstPeriodistas.Items.Add(periodista.Nombre);
-            }
-
-        }
-        catch (Exception ex)
-        {
-            lblMensaje.Text = ex.Message;
-        }
     }
 }
